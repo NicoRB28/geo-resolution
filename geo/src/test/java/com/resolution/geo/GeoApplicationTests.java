e811d61fe6e5d6713d7d93e1e936f5a532e3210c
nico <nicolasrodriguezboschi@gmail.com>
2021-09-03 20:28:18 -0300

    first commit

---
+++ geo/src/test/java/com/resolution/geo/GeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resolution.geo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
