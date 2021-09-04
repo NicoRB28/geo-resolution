1c159b3dfc09164dcf48e882b6e327af350c9c2e
nico <nicolasrodriguezboschi@gmail.com>
2021-09-04 01:23:45 -0300

    change return type in locationService + test functionality LocationService.getLocationByPosition

--- geo/src/main/java/com/resolution/geo/repository/LocationRepository.java
+++ geo/src/main/java/com/resolution/geo/repository/LocationRepository.java
@@ -1,10 +1,12 @@
 package com.resolution.geo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.resolution.geo.model.Location;
 
 public interface LocationRepository extends CrudRepository<Location, Long> {
 	
-	public Location findByLatAndLng(String lat, String lng);
+	public Optional<Location> findByLatAndLng(String lat, String lng);
 }
